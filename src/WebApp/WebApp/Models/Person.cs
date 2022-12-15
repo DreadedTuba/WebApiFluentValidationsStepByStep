a1e5c645983b12d25f387113519467920bf78deb
Sam Walker <swalker@rxsense.com>
2022-12-15 11:54:44 -0600

    Step 3: Add ValidationAttribute's to Person model.
    This works out of the box with WebApi and MVC. It is good for simple validations for MVC or Api models.  There is no response about what validations failed, actually the caller has no details if the call failed or success.

--- src/WebApp/WebApp/Models/Person.cs
+++ src/WebApp/WebApp/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace WebApp.Models
 {
     public class Person
     {
+        [StringLength(10)]
         public string Name { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string Email { get; set; }
