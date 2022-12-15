3b298ba6386ca43756da336d7e149e555951d935
Sam Walker <swalker@rxsense.com>
2022-12-15 14:56:18 -0600

    Step 10: Add new model Address with Controller and Validator.
    The validator is automatically used because of AssemblyScanner

---
+++ src/WebApp/WebApp/Controllers/AddressesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class AddressesController : ApiController
+    {
+        // GET: api/Addresses
+        public IEnumerable<Address> Get()
+        {
+            return new Address[] {
+                new Address()
+                {
+                    Street = "123 Easy St",
+                    City = "Nowhere",
+                    State = "OfMind",
+                    PostalCode = "12345"
+                },
+                new Address()
+                {
+                    Street = "666 Elm St",
+                    City = "Amittyville",
+                    State = "Horror",
+                    PostalCode = "89989"
+                }
+            };
+        }
+
+        // GET: api/Addresses/5
+        public Address Get(int id)
+        {
+            return new Address()
+            {
+                Street = "666 Elm St",
+                City = "Amittyville",
+                State = "Horror",
+                PostalCode = "89989"
+            };
+        }
+
+        // POST: api/Addresses
+        public void Post([FromBody] Address value)
+        {
+        }
+
+        // PUT: api/Addresses/5
+        public void Put(int id, [FromBody] Address value)
+        {
+        }
+
+        // DELETE: api/Addresses/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/WebApp/WebApp/Validators/AddressValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApp.Models;
+
+namespace WebApp.Validators
+{
+    public class AddressValidator : AbstractValidator<Address>
+    {
+        public AddressValidator()
+        {
+            RuleFor(a => a.Street).NotEmpty().MaximumLength(33);
+            RuleFor(a => a.City).NotEmpty().MaximumLength(22);
+            RuleFor(a => a.State).NotEmpty().MaximumLength(11);
+            RuleFor(a => a.PostalCode).NotEmpty().Length(5);
+        }
+    }
+}
